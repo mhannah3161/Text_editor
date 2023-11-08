2ff000b90f2fc70eabadb824b817334102a5c47a
Michael Hannah <Michaelrkhannah@gmail.com>
2023-11-07 20:47:57 -0700

    Added asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,8 +28,13 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0,200]
+      })
+    ]
   })
 );
